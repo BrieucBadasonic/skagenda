0321d65e63c06467a91a4e43e9ef3a8d2ae27e13
Brieuc Labiouse <brieuclabiouse@gmail.com>
2021-04-26 23:17:07 +0200

    event#new and create are working, need to fix date and redirect

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,42 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
     @venue = Venue.new
+    # @event.venue.build
     @event.bands.build
   end
 
   def create
-    @event = Event.new(event_params)
+    if Venue.where(name: event_params[:venues][:name]).empty?
+      @venue = Venue.new(event_params[:venues])
+      @venue.save!
+    else
+      @venue = Venue.where(name: event_params[:venues][:name])[0]
+    end
+
+    @event = Event.new(date: event_params[:date], price: event_params[:price])
+    @event.user = current_user
+    @event.venue = @venue
+    @event.save!
+
+    event_params[:bands_attributes].each do |band|
+      @ts = Timeslot.new
+      @ts.event = @event
+      if Band.where(name: band[1][:name]).empty?
+        @band = Band.new(name: band[1][:name])
+        @band.save!
+      else
+        @band = Band.where(name: band[1][:name])[0]
+      end
+      @ts.band = @band
+      @ts.save!
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :price, venues: [:name, :address], bands_attributes: [:name])
   end
 end
+# , :bands_attributes[:name]
+# params[:event][:bands_attributes].keys.count
